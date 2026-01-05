f65b818aeb8c2c864fdeff51c9fd2412aff63a0b
Amaan <amaan14999@gmail.com>
2026-01-06 01:20:32 +0530

    add: model changes to Category model

--- src/main/java/com/amaan/expense_tracker/domain/Category.java
+++ src/main/java/com/amaan/expense_tracker/domain/Category.java
@@ -1,8 +1,10 @@
 package com.amaan.expense_tracker.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Entity
@@ -11,6 +13,11 @@ import java.util.UUID;
                 name = "uk_categories_user_name",
                 columnNames = {"user_id", "name"})
 )
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
 
     @Id
@@ -22,5 +29,20 @@ public class Category {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Column(name = "name", nullable = false, length = 80)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false, length = 20)
+    private CategoryType type;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    void prePersist() {
+        if(createdAt == null) createdAt = OffsetDateTime.now();
+    }
 
 }
