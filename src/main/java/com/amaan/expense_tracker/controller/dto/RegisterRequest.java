4aa814c336e80d42b5f68543f75e1ea9c3c9a59b
Amaan <amaan14999@gmail.com>
2026-01-10 04:56:10 +0530

    add: DTOs for login/register

---
+++ src/main/java/com/amaan/expense_tracker/controller/dto/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.amaan.expense_tracker.controller.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+}
