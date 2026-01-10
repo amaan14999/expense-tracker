66c2b19fe88c4a493653272fd62816566bf6aaf1
Amaan <amaan14999@gmail.com>
2026-01-10 14:06:53 +0530

    add: AuthService

---
+++ src/main/java/com/amaan/expense_tracker/service/AuthService.java
@@ -0,0 +1,41 @@
+package com.amaan.expense_tracker.service;
+
+import com.amaan.expense_tracker.domain.User;
+import com.amaan.expense_tracker.repository.UserRepository;
+import com.amaan.expense_tracker.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public void register(String email, String password, String name) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        User user = User.builder()
+                .id(UUID.randomUUID())
+                .email(email)
+                .passwordHash(passwordEncoder.encode(password))
+                .name(name)
+                .build();
+
+        userRepository.save(user);
+    }
+
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Email does not exists!"));
+        if (!passwordEncoder.matches(password, user.getPasswordHash())) {
+            throw new IllegalArgumentException("Invalid password");
+        }
+        return jwtUtil.generateToken(user.getId(), user.getEmail());
+    }
+}
